76a885d9535bb1ab96bb64aeb120f30c0b716b14
Jorge Gingsberg <jorgegingsberg@gmail.com>
2020-03-27 01:19:30 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
-board = [" ","X"," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 
 
 def display_board(board)
   puts "   |   |   "
   puts "-----------"
-  puts "   |   |   "
+  puts "   | #{X}  |   "
   puts "-----------"
   puts "   |   |   "
 end
